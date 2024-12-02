3c2b5c649732f0864f4401c1ea9829c5f4c14fc2
bp42s <bransonyflynn@gmail.com>
2024-12-02 16:23:55 -0500

    added existing files from previous days

---
+++ Day01/Program.cs
@@ -0,0 +1,111 @@
+﻿using System.Linq;
+
+namespace AdventOfCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            Console.WriteLine("[Advent Of Code - Day 01]");
+
+            int answerPart1 = Parts.Answer1();
+            Console.WriteLine("Part 1 Answer: " + answerPart1);
+
+            int answerPart2 = Parts.Answer2();
+            Console.WriteLine("Part 2 Answer: " + answerPart2);
+
+            watch.Stop();
+            Console.WriteLine($"The Execution time of the program is {watch.ElapsedMilliseconds}ms");
+        }
+    }
+
+    internal static class Parts
+    {
+        public static int Answer1()
+        {
+            int answer = 0;
+            int[] listLeft = new int[1000];
+            int[] listRight = new int[1000];
+            var linesRead = File.ReadLines("input.txt");
+            try
+            {
+                int index = 0;  // counter for within foreach loop
+                foreach (var line in linesRead)
+                {
+                    string[] elems = line.Split("   ");
+                    listLeft[index] = Convert.ToInt32(elems[0]);
+                    listRight[index] = Convert.ToInt32(elems[1]);
+                    index++;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error reading input.txt");
+                return -1;
+            }
+
+            Array.Sort(listLeft);
+            Array.Sort(listRight);
+            for (int i = 0; i < listLeft.Length; i++)
+            {
+                int difference = Math.Abs(listLeft[i] - listRight[i]);
+                answer += difference;
+            }
+
+            return answer;
+        }
+
+        public static int Answer2()
+        {
+            int answer = 0;
+            int[] listLeft = new int[1000];
+            int[] listRight = new int[1000];
+            var linesRead = File.ReadLines("input.txt");
+            try
+            {
+                int index = 0;  // counter for within foreach loop
+                foreach (var line in linesRead)
+                {
+                    string[] elems = line.Split("   ");
+                    listLeft[index] = Convert.ToInt32(elems[0]);
+                    listRight[index] = Convert.ToInt32(elems[1]);
+                    index++;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error reading input.txt");
+                return -1;
+            }
+
+            var dictLeft = new Dictionary<int, int>();
+            foreach (var num in listLeft)
+            {
+                if (dictLeft.ContainsKey(num))
+                    dictLeft[num]++;
+                else
+                    dictLeft.TryAdd(num, 1);
+            }
+
+            var dictRight = new Dictionary<int, int>();
+            foreach (var num in listRight)
+            {
+                if (dictRight.ContainsKey(num))
+                    dictRight[num]++;
+                else
+                    dictRight.TryAdd(num, 1);
+            }
+
+            foreach (KeyValuePair<int, int> leftK in dictLeft)
+            {
+                if (dictRight.ContainsKey(leftK.Key))
+                {
+                    answer += leftK.Key * dictRight[leftK.Key]; // mult by rightCount
+                }
+            }
+            return answer;
+        }
+    }
+}
---
+++ Day02/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace AdventOfCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[Advent Of Code - Day 02]");
+
+            int answer1 = Part.Answer1();
+            Console.WriteLine("Part 1 Answer: " + answer1);
+        }
+    }
+
+    internal static class Part
+    {
+        public static int Answer1()
+        {
+            int safeReports = 0;
+            var linesRead = File.ReadLines("input.txt");
+            foreach (var line in linesRead)
+            {
+                string[] elemsStr = line.Split(" ");
+                Console.WriteLine("elemstr done");
+                int[] elems = Array.ConvertAll(elemsStr, int.Parse);
+                Console.WriteLine("convert done");
+                if (IsSafe(elems))
+                {
+                    safeReports++;
+                }
+            }
+
+            return safeReports;
+        }
+
+        public static bool IsSafe(int[] arr)
+        {
+            if (IsArrayIncreasingOrDecreasing(arr) && IsChangeRateValid(arr))
+                return true;
+            else
+                return false;
+        }
+        public static bool IsArrayIncreasingOrDecreasing(int[] arr)
+        {
+            // todo/note - this might not work if reports can increase+decrease levels in the same report
+            // int len = arr.Length;
+            // if (arr[0] <= arr[1] && arr[len - 2] <= arr[len - 1])
+            //     return true;
+            // else if (arr[0] >= arr[1] && arr[len - 2] >= arr[len - 1])
+            //     return true;
+            // else
+            //     return false;
+
+
+            // -1 = unset, 0 = decreasing, 1 = increasing
+            int mode;
+            if (arr[0] > arr[1])
+            {
+                // decreasing
+                mode = 0;
+            }
+            else if (arr[0] < arr[1])
+            {
+                // increasing
+                mode = 1;
+            }
+            else
+            {
+                // no change, return false early
+                return false;
+            }
+
+            for (int i = 0; i < arr.Length - 1; i++) { }
+
+            return false;
+        }
+
+        public static bool IsChangeRateValid(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (Math.Abs(arr[i] - arr[i + 1]) < 1 && Math.Abs(arr[i] - arr[i + 1]) > 3)
+                {
+                    return false;
+                }
+                i++;
+            }
+            return true;
+        }
+    }
+}
